8e4b3fee4c1562c14dcb3be2d124c6b0a85faad0
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-12 19:16:06 -0300

    1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+# Lendo a velocidade em m/s do usuário
+velocidade_ms = float(input("Digite a velocidade em m/s: "))
+
+# Calculando a velocidade em km/h
+velocidade_kmh = velocidade_ms * 3.6
+
+# Escrevendo a velocidade em km/h
+print("A velocidade é de", velocidade_kmh, "km/h")
