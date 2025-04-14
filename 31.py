56231e8e3e24f2d52c4d701965ae7febe202e021
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:34:01 -0300

    31.py

---
+++ 31.py
@@ -0,0 +1,5 @@
+numero_binario = input("Digite um número binário de 4 dígitos: ")
+
+numero_decimal = int(numero_binario, 2)
+
+print(f"O equivalente decimal de {numero_binario} é {numero_decimal}.")
