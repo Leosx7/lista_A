0945766f74659bae74630285965875ddae4a9b6a
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:23:57 -0300

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Digite o numero 1 : '))
+numero2 = int(input('Digite o numero 2 : '))
+
+quociente_divisao = numero1 // numero2
+resto_divisao = numero1 % numero2
+
+print(f'O quociente(resultado) da divisão dos dois numeros é { quociente_divisao} e o resto da mesma é : {resto_divisao}')
