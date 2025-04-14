87e8afa15be8bf2ad06f546629fba4e38337c540
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:31:19 -0300

    25.py

---
+++ 25.py
@@ -0,0 +1,6 @@
+valor_em_metros = int(input('Digite o valor em metros : '))
+
+quilometros = valor_em_metros // 1000
+metros = valor_em_metros % 1000
+
+print(f'O valor em quilometros e metros é : {quilometros} km  e {metros} m ')
