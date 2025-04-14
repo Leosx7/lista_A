261afc7667747d6fd8c5be671e07eb14abb43266
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:28:39 -0300

    18.py

---
+++ 18.py
@@ -0,0 +1,6 @@
+raio_circuferencia = float(input('Digite o valor do raio da circuferência : '))
+
+pi = 3.14
+comprimento_circuferencia = 2 * pi * raio_circuferencia
+
+print(f'O valor do comprimento da circuferência é : {comprimento_circuferencia:.2f}')
