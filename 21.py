491cf570d3d522e276ecfcd7ed23377906694a00
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:29:47 -0300

    21.py

---
+++ 21.py
@@ -0,0 +1,5 @@
+temperatura_em_f = float(input('Digite o valor da temperatura em ºF : '))
+
+temperatura_em_celcius = (5 * temperatura_em_f - 160) / 9
+
+print(f'A temperatura em ºC é : {temperatura_em_celcius:.2f}')
