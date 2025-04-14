cd1ea78159ee477ec56ab5dd92cf6594c5c05ecc
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:26:04 -0300

    13.py

---
+++ 13.py
@@ -0,0 +1,5 @@
+valor_em_real = int(input('Digite o valor em real : '))
+
+setenta_porcento = valor_em_real * (70 / 100)
+
+print(f'O valor digitado em real(R$) é : {valor_em_real} e 70% do mesmo é : {setenta_porcento:.1f}')
