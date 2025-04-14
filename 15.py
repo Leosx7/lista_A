c9fd6c42799e6774aa005b292c015db8a4f63201
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:27:35 -0300

    15.py

---
+++ 15.py
@@ -0,0 +1,6 @@
+base_triangulo = float(input('Digite o valor da base do triângulo : '))
+altura_triangulo = float(input('Digite o valor da altura do triângulo : '))
+
+area_do_triangulo = (base_triangulo*altura_triangulo) / 2
+
+print(f'A area do triângulo é : {area_do_triangulo}')
