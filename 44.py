98cd04bc13cb01c79c1c0c1b3e3090330aeb9c05
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:39:44 -0300

    44.py

---
+++ 44.py
@@ -0,0 +1,7 @@
+# 70% d ecobre e 30% de zinco
+quantidade_latao = int(input('Digite a quantidade de latao( em kg) : '))
+
+qntd_cobre = quantidade_latao * 0.70
+qntd_zinco = quantidade_latao * 0.30
+
+print(f' A quantidade de cobre e zinco usados para a fabricacapo do latao respectivamente é :{qntd_cobre} kg  {qntd_zinco} kg')
