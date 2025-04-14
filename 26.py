4224c381d7fa34f24f71e9b3670391404440fbe1
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:31:49 -0300

    26.py

---
+++ 26.py
@@ -0,0 +1,6 @@
+dias = int(input('Digite a quantidade de dias : '))
+
+semanas = dias // 7
+resto_dias = dias % 7
+
+print(f'A quantidade de dias corresponde à : {semanas} semanas e {resto_dias} dias')
