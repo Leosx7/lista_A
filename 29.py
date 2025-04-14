48d59eadb1e81539a2b9c947b5f0b9b332e1427c
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:33:14 -0300

    29.py

---
+++ 29.py
@@ -0,0 +1,6 @@
+meses = int(input('Digite a quantidade de meses : '))
+
+anos = meses // 12
+resto_meses = meses % 12
+
+print(f'A quantidade de meses corresponde à : {anos} anos e {resto_meses} meses')
