7ce9dbe3af914679e80225feed6295afd5b54595
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:23:13 -0300

    8.py

---
+++ 8.py
@@ -0,0 +1,10 @@
+numero1 = int(input('Digite o numero 1 : '))
+numero2 = int(input('Digite o numero 2 : '))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+divisao_soma_pela_subtracao = soma // subtracao
+
+print(f'A soma dos dois numeros é : {soma}')
+print(f'A subtração dos dois numeros é : {subtracao}')
+print(f'A divisão da soma e subtração dos dois numeros é : {divisao_soma_pela_subtracao}')
