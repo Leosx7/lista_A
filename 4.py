7da5d80c1427178bc305d4c36b1fc9dc97540369
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:20:32 -0300

    4.py

---
+++ 4.py
@@ -0,0 +1,6 @@
+valor_do_dolar = float(input('Digite o valor do dolar : '))
+valor_em_dolar = float(input('Digite o valor em dolar : '))
+
+conversao_real = valor_do_dolar * valor_em_dolar
+
+print(f'O valor em dolar é : {valor_em_dolar} ,  e o mesmo em real é : {conversao_real}')
