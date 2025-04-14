72c07cb1a2a36b7f7a48aa8fc772f6d49c2405ba
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:28:18 -0300

    17.py

---
+++ 17.py
@@ -0,0 +1,6 @@
+base_retangulo = float(input('Digite o valor da base do retângulo : '))
+altura_retangulo = float(input('Digite o valor da altura do retângulo : '))
+
+area_retangulo = base_retangulo * altura_retangulo
+
+print(f'O valor da are do retângulo é : {area_retangulo}')
