f4fd67702c7bd6d2b9b6e7b980cfeeb57552f807
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:22:51 -0300

    7.py

---
+++ 7.py
@@ -0,0 +1,8 @@
+numero1 = int(input('Digite o numero 1 : '))
+numero2 = int(input('Digite o numero 2 : '))
+numero3 = int(input('Digite o numero 3 : '))
+
+soma = numero1 + numero2
+subtracao = numero2 - numero3
+
+print(f'A soma dos dois primeiros numeros é : {soma}  e a subtração dos dois últimos é : {subtracao}')
