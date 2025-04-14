391e28ef35c1f93227fb24932699364701e482e9
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:29:05 -0300

    19.py

---
+++ 19.py
@@ -0,0 +1,6 @@
+raio_esfera = float(input('Digite o valor do raio da esfera : '))
+
+pi = 3.14
+volume_esfera = (4 * pi * raio_esfera**3) /  3
+
+print(f'O valor do volume da esfera é : {volume_esfera:.2f}')
