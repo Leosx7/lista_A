80ceda94a0d535108acb05583ae7b5dd7fc24bf9
Leosx7 <160680267+Leosx7@users.noreply.github.com>
2025-04-14 10:35:20 -0300

    34.py

---
+++ 34.py
@@ -0,0 +1,7 @@
+numero_1 = float(input('Digite o valor do número 1 : '))
+numero_2 = float(input('Digite o valor do número 2 : '))
+numero_3 = float(input('Digite o valor do número 3 : '))
+
+media = (numero_1 + numero_2 + numero_3) / 3
+
+print(f'A média entre os números é : {media}')
